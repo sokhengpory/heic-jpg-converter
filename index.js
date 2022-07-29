8641b34d3562ad5b20ef66e0e027a1ac1aa55930
Sokheng <porysokheng@gmail.com>
2022-07-29 13:16:11 +0700

    minor refactor

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ const heicDirectory = path.join(__dirname, 'HEIC');
 
 const heicImageArray = fs.readdirSync(heicDirectory);
 
-const main = async () => {
+function formatDate(dateTime) {
+  const newDate = new Date(dateTime);
+  const date = newDate.toISOString().split('T')[0].replaceAll('-', ':');
+  const time = newDate.toTimeString().split(' ')[0];
+
+  return `${date} ${time}`;
+}
+
+(async function () {
   for (const image of heicImageArray) {
     const filename = image.split('.')[0];
     const heicImagePath = path.join(__dirname, 'HEIC', image);
     const jpgImagePath = path.join(__dirname, 'JPG', `${filename}.jpg`);
+
     const buffer = await promisify(fs.readFile)(heicImagePath);
     const jpgBuffer = await convert({
       buffer,
@@ -24,10 +33,7 @@ const main = async () => {
     console.log(`${image} -> ${filename}.jpg: Done!`);
   }
   console.log('Convert all HEIC to JPG: Done!');
-  insertMetadata();
-};
 
-const insertMetadata = async () => {
   for (const image of heicImageArray) {
     const filename = image.split('.')[0];
     const jpgPath = path.join(__dirname, 'JPG', `${filename}.jpg`);
@@ -38,37 +44,17 @@ const insertMetadata = async () => {
       true
     );
 
-    const zeroth = {};
-    const exif = {};
-    const gps = {};
+    const dateTime = formatDate(heicMetadata.DateTimeOriginal);
 
-    const date = heicMetadata.DateTimeOriginal.toLocaleString()
-      .split(',')[0]
-      .replaceAll('/', ':')
-      .split(':')
-      .reverse();
-    [date[1], date[2]] = [date[2], date[1]];
-
-    const time = heicMetadata.DateTimeOriginal.toLocaleString()
-      .split(', ')[1]
-      .split(' ');
-
-    const convertHourFromTime = (time) => {
-      if (time[1] == 'PM') {
-        const newHour = +time[0].split(':')[0] + 12 + '';
-        const oldHour = time[0].split(':')[0];
-        const newTime = time[0].replace(oldHour, newHour);
-        return newTime;
-      }
+    const exif = {
+      [piexif.ExifIFD.DateTimeOriginal]: dateTime,
+    };
+    const zeroth = {
+      [piexif.ImageIFD.Make]: heicMetadata.Make,
+      [piexif.ImageIFD.Model]: heicMetadata.Model,
     };
 
-    const dateTime = `${date.join(':')} ${convertHourFromTime(time)}`;
-
-    exif[piexif.ExifIFD.DateTimeOriginal] = dateTime;
-    zeroth[piexif.ImageIFD.Make] = heicMetadata.Make;
-    zeroth[piexif.ImageIFD.Model] = heicMetadata.Model;
-
-    const exifObj = { '0th': zeroth, Exif: exif, GPS: gps };
+    const exifObj = { '0th': zeroth, Exif: exif };
     const exifbytes = piexif.dump(exifObj);
 
     const newData = piexif.insert(exifbytes, jpegData);
@@ -77,6 +63,4 @@ const insertMetadata = async () => {
     fs.writeFileSync(jpgPath, newJpeg);
   }
   console.log('Insert metadata from HEIC image to converted JPG image: Done!');
-};
-
-main();
+})();
